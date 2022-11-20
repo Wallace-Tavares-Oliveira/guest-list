8387a8ba4ecbf40c9ef288a2d045a0b16ebc76ff
Wallace <wallacetavaresoliveira84@gmail.com>
2022-11-19 21:08:23 -0300

    Project created

---
+++ src/main/java/com/devsolo/guestList/GuestListApplication.java
@@ -0,0 +1,13 @@
+package com.devsolo.guestList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsolo/guestList/GuestListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsolo.guestList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
